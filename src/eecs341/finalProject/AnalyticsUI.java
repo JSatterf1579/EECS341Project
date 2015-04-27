84d4d2cece2ce6efb3386c90ec3b8de5171d8b31
freshvictory <jsren1@verizon.net>
2015-04-27 03:01:19 -0400

    updated analytics

--- src/eecs341/finalProject/AnalyticsUI.java
+++ src/eecs341/finalProject/AnalyticsUI.java
@@ -32,14 +32,35 @@ public class AnalyticsUI extends JFrame {
 
 		JButton back = new JButton("Back");
 		JButton execute = new JButton("Execute");
-		back.setBounds(20, 300, 120, 40);
-		execute.setBounds(160, 300, 120, 40);
+		back.setBounds(20, 330, 120, 40);
+		execute.setBounds(160, 330, 120, 40);
 		frame.add(back);
 		frame.add(execute);
 		
-		JTable resultTable = new JTable();
-		resultTable.setBounds(20,160,260,120);
-		frame.add(resultTable);
+		JButton query1 = new JButton("Query 1");
+		JButton query2 = new JButton("Query 2");
+		JButton query3 = new JButton("Query 3");
+		JButton query4 = new JButton("Query 4");
+		JButton query5 = new JButton("Query 5");
+		JButton query6 = new JButton("Query 6");
+		JButton query7 = new JButton("Query 7");
+		JButton query8 = new JButton("Query 8");
+		query1.setBounds(300, 20, 380, 30);
+		frame.add(query1);
+		query2.setBounds(300, 60, 380, 30);
+		frame.add(query2);
+		query3.setBounds(300, 100, 380, 30);
+		frame.add(query3);
+		query4.setBounds(300, 140, 380, 30);
+		frame.add(query4);
+		query5.setBounds(300, 180, 380, 30);
+		frame.add(query5);
+		query6.setBounds(300, 220, 380, 30);
+		frame.add(query6);
+		query7.setBounds(300, 260, 380, 30);
+		frame.add(query7);
+		query8.setBounds(300, 300, 380, 30);
+		frame.add(query8);
 		
 		JTextArea queryLabel = new JTextArea("Enter Query:");
 		queryLabel.setEditable(false);
@@ -47,11 +68,11 @@ public class AnalyticsUI extends JFrame {
 		frame.add(queryLabel);
 		JTextArea query = new JTextArea();
 		JScrollPane queryScroll = new JScrollPane(query);
-		query.setBounds(20, 40, 260, 100);
-		queryScroll.setBounds(20, 40, 260, 100);
+		query.setBounds(20, 40, 260, 270);
+		queryScroll.setBounds(20, 40, 260, 270);
 		query.setLineWrap(true);
 		frame.add(queryScroll);
-		frame.setSize(300, 400);
+		frame.setSize(700, 400);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.getContentPane().setBackground(Color.white);
@@ -68,7 +89,7 @@ public class AnalyticsUI extends JFrame {
 				ResultSet rs;
 				try {
 					rs = db.runQueryString(query.getText());
-					resultTable.setModel(buildTableModel(rs));
+					new ResultUI(db, rs);
 				} catch (SQLConnectionException e) {
 					new PopupUI(e.toString(), e.getMessage());
 				} catch (SQLException e) {
@@ -76,26 +97,54 @@ public class AnalyticsUI extends JFrame {
 				}
 			}
 		});
-	}
-	
-	private DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
-	    ResultSetMetaData metaData = rs.getMetaData();
-	    // names of columns
-	    Vector<String> columnNames = new Vector<String>();
-	    int columnCount = metaData.getColumnCount();
-	    for (int column = 1; column <= columnCount; column++) {
-	        columnNames.add(metaData.getColumnName(column));
-	    }
-	    // data of the table
-	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-	    while (rs.next()) {
-	        Vector<Object> vector = new Vector<Object>();
-	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-	            vector.add(rs.getObject(columnIndex));
-	        }
-	        data.add(vector);
-	    }
-	    return new DefaultTableModel(data, columnNames);
+		
+		query1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				// TODO: SQL RUN QUERY
+			}
+		});
+		
+		query2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				// TODO: SQL RUN QUERY
+			}
+		});
+		
+		query3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				// TODO: SQL RUN QUERY
+			}
+		});
+		
+		query4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				// TODO: SQL RUN QUERY
+			}
+		});
+		
+		query5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				// TODO: SQL RUN QUERY
+			}
+		});
+		
+		query6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				// TODO: SQL RUN QUERY
+			}
+		});
+		
+		query7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				// TODO: SQL RUN QUERY
+			}
+		});
+		
+		query8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				// TODO: SQL RUN QUERY
+			}
+		});
 	}
 
 }
